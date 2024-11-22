d9c512ab16b5ea7ba9566b7a427f00bf88a6da3b
Roman <christmasspb@gmail.com>
2024-11-22 21:33:49 +0300

    2_hw.py has been added

---
+++ 2_hw.py
@@ -0,0 +1,17 @@
+#Задача 1
+def task_1(a: int = 1, b: float = 2.5, c: str = 'three', d: list = [4, 5, 6], e: bool = True) -> int:
+    print("int, float, str, list, bool")
+    return a, b, c, d, e
+print(task_1())
+
+#Задача 2
+a = [1, 2, 3, 5, 8, 13, 21]
+def task_2(a: list) -> int:
+    return a[0:3]
+print(task_2(a))
+#d. эта последовательность чисел называется списком
+
+#Задача 3
+def task_3(a: int = 5) -> int:
+    return a**2
+print(task_3())
